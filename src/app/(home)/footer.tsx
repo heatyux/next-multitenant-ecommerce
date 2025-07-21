c7c5cd962439bb980aa6cbc4898dee1524d38e0a
heatyux <421743974@qq.com>
2025-07-21 14:44:28 +0800

    feat: implemented footer component

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <footer className="flex justify-between border-t p-6 font-medium">
+      <div>
+        <div className="flex items-center gap-2">
+          <p>funroad, Inc.</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,12 +1,16 @@
 import { PropsWithChildren } from 'react'
 
+import { Footer } from './footer'
 import { Navbar } from './navbar'
 
 const HomeLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
-      {children}
+
+      <div className="flex-1 bg-[#F4F4F0]">{children}</div>
+
+      <Footer />
     </div>
   )
 }
