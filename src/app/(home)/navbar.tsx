6d2a60335a74c70546e269a04892b6f118d046a7
heatyux <421743974@qq.com>
2025-07-21 14:18:47 +0800

    feat: add authentication buttons to the navbar for login and registration

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -67,6 +67,24 @@ export const Navbar = () => {
           </NavbarItem>
         ))}
       </div>
+
+      {/* Auth buttons */}
+      <div className="hidden h-full lg:flex">
+        <Button
+          variant="secondary"
+          className="h-full rounded-none border-t-0 border-r-0 border-b-0 border-l bg-white px-12 text-lg transition-colors hover:bg-pink-400"
+          asChild
+        >
+          <Link href="/sign-in">Log in </Link>
+        </Button>
+        <Button
+          variant="secondary"
+          className="h-full rounded-none border-t-0 border-r-0 border-b-0 border-l bg-black px-12 text-lg text-white transition-colors hover:bg-pink-400 hover:text-black"
+          asChild
+        >
+          <Link href="/sign-up">Start selling</Link>
+        </Button>
+      </div>
     </nav>
   )
 }
