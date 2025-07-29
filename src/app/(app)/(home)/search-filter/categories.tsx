0781bec9451697797ae61debbe85b67a0e76278f
heatyux <421743974@qq.com>
2025-07-29 14:59:08 +0800

    refactor: restructure categories component for improved layout and readability

--- src/app/(app)/(home)/search-filter/categories.tsx
+++ src/app/(app)/(home)/search-filter/categories.tsx
@@ -9,16 +9,18 @@ type CategoriesProps = {
 // Categories - Renders a list of categories dropdown buttons for filtering
 export const Categories = ({ data }: CategoriesProps) => {
   return (
-    <div>
-      {data.map((category: Category) => (
-        <div key={category.id}>
-          <CategoryDropdown
-            category={category}
-            isActive={false}
-            isNavigationHovered={false}
-          />
-        </div>
-      ))}
+    <div className="relative w-full">
+      <div className="flex flex-nowrap items-center">
+        {data.map((category: Category) => (
+          <div key={category.id}>
+            <CategoryDropdown
+              category={category}
+              isActive={false}
+              isNavigationHovered={false}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
